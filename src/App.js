f75f1a3e53d7e7a2e28426596cc7d4e63771ad86
Wes Reid <bwreid@gmail.com>
2018-05-08 08:38:59 -0700

    Remove semicolons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
+import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.css'
 
 class App extends Component {
   render() {
-    return (<div>Hello React</div>);
+    return (<div>Hello React</div>)
   }
 }
 
-export default App;
+export default App
